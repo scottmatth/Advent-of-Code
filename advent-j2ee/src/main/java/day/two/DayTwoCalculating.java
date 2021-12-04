0c7a639e589472670d1eaab94e614cb2964734af
Scott Matthews <scottmat@broadinstitute.org>
2021-12-04 01:09:46 -0500

    Day 2 part 1 completed.

--- advent-j2ee/src/main/java/day/two/DayTwoCalculating.java
+++ advent-j2ee/src/main/java/day/two/DayTwoCalculating.java
@@ -25,16 +25,22 @@ public class DayTwoCalculating {
             this.referencedDirection = referencedDirection;
         }
 
-        public static void moveForward(int distance) {
+        public void moveForward(int distance) {
+            System.out.printf("Moving Forward: Old Horizontal is %s, increasing by %s\n",
+                    divePositions.get(DataUtility.Positions.HORIZONTAL), distance);
             divePositions.merge(DataUtility.Positions.HORIZONTAL,distance, Integer::sum);
         }
 
-        public static void moveUp(int distance) {
-            divePositions.merge(DataUtility.Positions.VERTICAL, distance, Integer::sum);
+        public void moveUp(int distance) {
+            System.out.printf("Moving Up: Old Vertical is %s, decreasing by %s\n",
+                    divePositions.get(DataUtility.Positions.VERTICAL), distance);
+            divePositions.merge(DataUtility.Positions.VERTICAL, -distance, Integer::sum);
         }
 
-        public static void moveDown(int distance) {
-            divePositions.merge(DataUtility.Positions.VERTICAL, -distance, Integer::sum);
+        public void moveDown(int distance) {
+            System.out.printf("Moving Up: Old Vertical is %s, increasing by %s\n",
+                    divePositions.get(DataUtility.Positions.VERTICAL), distance);
+            divePositions.merge(DataUtility.Positions.VERTICAL, distance, Integer::sum);
         }
 
         public String getReferencedDirection() {
@@ -42,20 +48,37 @@ public class DayTwoCalculating {
         }
 
         public static PositionTypes fromReference(String currentDirection) {
+            PositionTypes foundType= null;
             for (PositionTypes value : values()) {
-                if(StringUtils value.referencedDirection)
+                if(value.referencedDirection.equals(currentDirection)) {
+                    foundType = value;
+                    break;
+                }
             }
-
+            return foundType;
         }
     }
     public static void main(String[] args) {
 
         for (String divePosition : DataUtility.divePositions) {
             String[] splitValues = divePosition.split(" ");
-            switch (splitValues[0]) {
 
+            PositionTypes positionType = PositionTypes.fromReference(splitValues[0]);
+            switch (positionType) {
+            case Up:
+                positionType.moveUp(Integer.parseInt(splitValues[1]));
+                break;
+            case Down:
+                positionType.moveDown(Integer.parseInt(splitValues[1]));
+                break;
+            case Forward:
+                positionType.moveForward(Integer.parseInt(splitValues[1]));
+                break;
             }
         }
 
+        System.out.printf("The final horizontal is %s.  The final vertical is %s.  The multiplied values are %s.\n",
+                divePositions.get(DataUtility.Positions.HORIZONTAL), divePositions.get(DataUtility.Positions.VERTICAL),
+                divePositions.get(DataUtility.Positions.HORIZONTAL) * divePositions.get(DataUtility.Positions.VERTICAL));
     }
 }
